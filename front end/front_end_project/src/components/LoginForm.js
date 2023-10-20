437afc34cc8d14de7c4a1d7c31c589e76792463d
Arujazzz <aruzhankubaeva03@gmail.com>
2023-10-21 01:05:49 +0600

    add some css for registration

--- front end/front_end_project/src/components/LoginForm.js
+++ front end/front_end_project/src/components/LoginForm.js
@@ -44,7 +44,7 @@ const LoginForm = ({ toggleLoginForm }) => {
         <div class="shape"></div>
         <div class="shape"></div>
       </div>
-      <form onSubmit={handleLogin}>
+      <form class="loginform" onSubmit={handleLogin}>
       <h3>Login Here</h3>
         <div>
           <label htmlFor="email">Email:</label>
@@ -75,26 +75,6 @@ const LoginForm = ({ toggleLoginForm }) => {
         </div>
       </form>
     </div>
-   
-    // <div class="background">
-    //     <div class="shape"></div>
-    //     <div class="shape"></div>
-    // </div>
-    // <form>
-    //     <h3>Login Here</h3>
-
-    //     <label for="username">Username</label>
-    //     <input type="text" placeholder="Email or Phone" id="username">
-
-    //     <label for="password">Password</label>
-    //     <input type="password" placeholder="Password" id="password">
-
-    //     <button>Log In</button>
-        // <div class="social">
-        //   <div class="go"><i class="fab fa-google"></i>  Google</div>
-        //   <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
-        // </div>
-    // </form>
   
   );
 };
--- front end/front_end_project/src/components/RegisterForm.js
+++ front end/front_end_project/src/components/RegisterForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import './RegisterForm.css';
 
 const RegisterForm = ({ toggleRegisterForm }) => {
   const [formData, setFormData] = useState({
@@ -60,140 +60,149 @@ const RegisterForm = ({ toggleRegisterForm }) => {
 
   return (
     <div>
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="first_name">First Name:</label>
-        <input
-          type="text"
-          id="first_name"
-          name="first_name"
-          value={formData.first_name}
-          onChange={handleChange}
-          required
-        />
+      <div class="backgroundregister">
+        <div class="reshape"></div>
+        <div class="reshape"></div>
       </div>
-      <div>
-        <label htmlFor="last_name">Last Name:</label>
-        <input
-          type="text"
-          id="last_name"
-          name="last_name"
-          value={formData.last_name}
-          onChange={handleChange}
-          required
-        />
+      <form className='registerform' onSubmit={handleSubmit}>
+      <h3>Register Here</h3>
+      <div className='firstcontainer'>
+        <div>
+          <label className='firstname' htmlFor="first_name">First Name:</label>
+          <input
+            type="text"
+            id="first_name"
+            name="first_name"
+            value={formData.first_name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label className='lastname' htmlFor="last_name">Last Name:</label>
+          <input
+            type="text"
+            id="last_name"
+            name="last_name"
+            value={formData.last_name}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label className='email' htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label className='pass' htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label className='confirmpass' htmlFor="confirmPassword">confirmPassword:</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleChange}
+            required
+          />
+        </div>  
       </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
+      <div className='secondcontainer'>
+        <div>
+          <label className='avatar' htmlFor="AvatarUrl">avatarUrl:</label>
+          <input
+            type="file"
+            id="AvatarUrl"
+            name="AvatarUrl"
+            value={formData.avatarUrl}
+            onChange={handleChange}
+            required
+          />
+        </div> 
+        <div>
+          <label className='usertag' htmlFor="usertag">usertag:</label>
+          <select
+          id="usertag"
+          name="usertag"
+          value={formData.usertag}
           onChange={handleChange}
           required
-        />
-      </div>
-      <div>
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword">confirmPassword:</label>
-        <input
-          type="password"
-          id="confirmPassword"
-          name="confirmPassword"
-          value={formData.confirmPassword}
-          onChange={handleChange}
-          required
-        />
+          >
+          <option value="td">td</option>
+          <option value="ms">ms</option>
+          <option value="jb">jb</option>
+          <option value="bj">bj</option>
+          <option value="dg">dg</option>
+          <option value="dm">dm</option>
+          <option value="string">string</option>
+          <option value="jm">jm</option>
+          <option value="Tech Lead">Tech Lead</option>
+          <option value="ct">ct</option>
+          </select>
+        </div>
+        <div>
+          <label className='department' htmlFor="department">Department:</label>
+          <select
+            type="text"
+            id="department"
+            name="department"
+            value={formData.department}
+            onChange={handleChange}
+            required
+          >
+          <option value="IOS">IOS</option>
+          <option value="Android">Android</option>
+          <option value="Manager">Manager</option>
+          <option value="Analyst">Analyst</option>
+          <option value="Designer">Designer</option>
+          </select>
+        </div> 
+        <div>
+          <label className='birthday' htmlFor="birthday">birthday:</label>
+          <input
+            type="data"
+            id="birthday"
+            name="birthday"
+            value={formData.birthday}
+            onChange={handleChange}
+            required
+            />
+        </div> 
+        <div>
+          <label className='phone' htmlFor="phone">phone:</label>
+          <input
+            type="text"
+            id="phone"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            required
+          />
+        </div>
       </div>  
-      <div>
-        <label htmlFor="AvatarUrl">avatarUrl:</label>
-        <input
-          type="file"
-          id="AvatarUrl"
-          name="AvatarUrl"
-          value={formData.avatarUrl}
-          onChange={handleChange}
-          required
-        />
-      </div> 
-      <div>
-      <label htmlFor="usertag">usertag:</label>
-      <select
-        id="usertag"
-        name="usertag"
-        value={formData.usertag}
-        onChange={handleChange}
-        required
-      >
-        <option value="td">td</option>
-        <option value="ms">ms</option>
-        <option value="jb">jb</option>
-        <option value="bj">bj</option>
-        <option value="dg">dg</option>
-        <option value="dm">dm</option>
-        <option value="string">string</option>
-        <option value="jm">jm</option>
-        <option value="Tech Lead">Tech Lead</option>
-        <option value="ct">ct</option>
-      </select>
-    </div>
-      <div>
-        <label htmlFor="department">Department:</label>
-        <select
-          type="text"
-          id="department"
-          name="department"
-          value={formData.department}
-          onChange={handleChange}
-          required
-        >
-        <option value="IOS">IOS</option>
-        <option value="Android">Android</option>
-        <option value="Manager">Manager</option>
-        <option value="Analyst">Analyst</option>
-        <option value="Designer">Designer</option>
-        
-      </select>
-
-
-
-
-      </div> 
-      <div>
-        <label htmlFor="birthday">birthday:</label>
-        <input
-          type="data"
-          id="birthday"
-          name="birthday"
-          value={formData.birthday}
-          onChange={handleChange}
-          required
-        />
-      </div> 
-      <div>
-        <label htmlFor="phone">phone:</label>
-        <input
-          type="text"
-          id="phone"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-          required
-        />
-      </div> 
-      <button type="submit">Register</button>
-    </form>
+       
+      <button className='buttonregister' type="submit">Register</button>
+      <div class="socialre">
+        <div class="go"><i class="fab fa-google"></i>  Google</div>
+        <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
+      </div>
+      </form>
     </div>
   );
 };
